9ad3e36322fcdcff1ce73f3da21b0cd52d95a8e8
Henaute <95632172+Henaute@users.noreply.github.com>
2021-12-08 18:06:19 +0100

    Add files via upload

--- Memouse.py
+++ Memouse.py
@@ -271,7 +271,7 @@ if __name__ == "__main__":
     if Base == None or (not os.path.isdir(Base)):
         Base = os.getcwd()
     
-    error_file = open(Base+'/error_file')
+    error_file = open(Base+'/error_file.txt')
     os.chmod(Base+'/error_file',0o666)
     
     BaseIN = os.path.join(Base,'raw')
@@ -317,6 +317,7 @@ if __name__ == "__main__":
             a.close()
             
             d=d+1
+    error_file.close()
             
     print("temps de convertion",tConv)
     print("temps de concatenation",tMerg)
